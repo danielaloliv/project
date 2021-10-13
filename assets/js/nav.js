4bfde30685bfb4a8749b96dcff621166578079bf
danielaloliv <danielaloliv@outlook.com>
2021-10-13 10:36:12 +0100

    teste share nativo 2.0

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -25,3 +25,38 @@ function hideMenu() {
 		null;
 	}
 }
+
+
+
+
+/*  */
+window.setTimeout( function(){
+
+	const shareButton = document.querySelector('.botaoespecial');
+    const overlay = document.querySelector('.overlay');
+    const shareModal = document.querySelector('.share');
+
+    const title = window.document.title;
+    const url = window.document.location.href;
+
+    shareButton.addEventListener('click', () => {
+        if (navigator.share) {
+            navigator.share({
+                title: `${title}`,
+                url: `${url}`
+            }).then(() => {
+                console.log('Thanks for sharing!');
+            })
+            .catch(console.error);
+        } else {
+            overlay.classList.add('show-share');
+            shareModal.classList.add('show-share');
+        }
+    })
+
+    overlay.addEventListener('click', () => {
+        overlay.classList.remove('show-share');
+        shareModal.classList.remove('show-share');
+    })
+
+}, 2000)
